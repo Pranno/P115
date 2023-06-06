53092a1e44ba4fece6e52b8bc1805e45790f8181
Pranno <101404297+Pranno@users.noreply.github.com>
2023-06-06 20:26:30 +0530

    Add files via upload

---
+++ p115.py
@@ -0,0 +1,7 @@
+import os
+
+source = "C:/Users/Pranjal Kadam/Desktop/White Hat Jr/Pranjal's Projects"
+dest = "C:/Users/Pranjal Kadam/Desktop/White Hat Jr/ Projects"
+
+os.rename(source, dest)
+print("Souce path renamed to destination path succesfully")
